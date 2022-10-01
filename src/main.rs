ea149bd43e8473eedc96c1098f9a0f39615e3383
vlad <0956458@hr.nl>
2022-10-01 15:21:00 +0200

    finisehd creating nested structures

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,20 @@
-use std::{error::Error, fmt::format, process::Command};
+use std::{error::Error, process::Command};
 
-
-
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Session {
     name: String,
     window_count: u8,
+    windows: Option<Vec<Window>>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Window {
     id: u8,
     name: String,
+    panes: Option<Vec<Pane>>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 struct Pane {
     id: u8,
     size: (u32, u32),
@@ -24,14 +24,7 @@ fn main() {
     let raw_sessions = get_tmux_info(&["list-sessions"]).unwrap();
     for session in raw_sessions {
         let session = extract_session(&session).unwrap();
-        let windows = extract_windows(&session).unwrap();
-        extract_panes(&session, &windows);
-        // println!("{} {} {:?}", session.name, session.window_count, windows);
     }
-    // let tt = get_tmux_info( &["list-windows", "-t", "rust"]);
-    // let binding = tt.unwrap();
-    // let t3 = binding.split('\n').collect::<Vec<_>>();
-    // println!("{:#?}", t3);
 }
 
 fn get_tmux_info(args: &[&str]) -> Result<Vec<String>, Box<dyn Error>> {
@@ -58,15 +51,17 @@ fn extract_session(input: &str) -> Result<Session, Box<dyn Error>> {
     let session_name = input[0]
         .trim_end_matches(|v| !char::is_alphabetic(v))
         .to_string();
-    // println!("{}", session_name);
+
+    let windows = extract_windows(&session_name).ok();
     Ok(Session {
         name: session_name,
         window_count: input[1].parse()?,
+        windows,
     })
 }
 
-fn extract_windows(session: &Session) -> Result<Vec<Window>, Box<dyn Error>> {
-    let raw_windows = get_tmux_info(&["list-windows", "-t", &session.name]).unwrap();
+fn extract_windows(session_name: &str) -> Result<Vec<Window>, Box<dyn Error>> {
+    let raw_windows = get_tmux_info(&["list-windows", "-t", session_name]).unwrap();
 
     let mut windows: Vec<Window> = vec![];
     for window in raw_windows {
@@ -83,40 +78,43 @@ fn extract_windows(session: &Session) -> Result<Vec<Window>, Box<dyn Error>> {
         let name = input[1]
             .trim_end_matches(|v| !char::is_alphabetic(v))
             .to_string();
-        windows.push(Window { name, id });
+
+        let panes = extract_panes(session_name, id).ok();
+        windows.push(Window { name, id, panes });
     }
     Ok(windows)
 }
 
-fn extract_panes(session: &Session, windows: &[Window]) {
-    for window in windows {
-        let window_arg = format!("{}:{}", session.name, window.name);
-        let panes = get_tmux_info(&["list-pane", "-t", &window_arg]).unwrap();
-        for pane in panes {
-            let input: [&str; 2] = pane
-                .split_whitespace()
-                .take(2)
-                .collect::<Vec<_>>()
-                .try_into()
-                .unwrap();
+fn extract_panes(session_name: &str, window_id: u8) -> Result<Vec<Pane>, Box<dyn Error>> {
+    let window_arg = format!("{}:{}", session_name, window_id);
+    let panes = get_tmux_info(&["list-pane", "-t", &window_arg]).unwrap();
+    let mut pane_vec: Vec<Pane> = vec![];
 
-            let id: u8 = input[0][0..input[0].len() - 1].parse().unwrap();
+    for pane in panes {
+        let input: [&str; 2] = pane
+            .split_whitespace()
+            .take(2)
+            .collect::<Vec<_>>()
+            .try_into()
+            .unwrap();
 
-            let name: [u32; 2] = input[1]
-                .chars()
-                .filter(|v| v.is_alphanumeric())
-                .collect::<String>()
-                .replace('x', " ")
-                .split(' ')
-                .map(|v| v.parse::<u32>().unwrap())
-                .collect::<Vec<u32>>()
-                .try_into()
-                .unwrap();
-            let t = Pane {
-                id,
-                size: (name[0], name[1]),
-            };
-            println!("{:?}", t);
-        }
+        let id: u8 = input[0][0..input[0].len() - 1].parse().unwrap();
+
+        let name: [u32; 2] = input[1]
+            .chars()
+            .filter(|v| v.is_alphanumeric())
+            .collect::<String>()
+            .replace('x', " ")
+            .split(' ')
+            .map(|v| v.parse::<u32>().unwrap())
+            .collect::<Vec<u32>>()
+            .try_into()
+            .unwrap();
+        pane_vec.push(Pane {
+            id,
+            size: (name[0], name[1]),
+        });
     }
+    Ok(pane_vec)
 }
+
